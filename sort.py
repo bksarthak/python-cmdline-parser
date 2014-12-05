1f151e9fd851ed9a1664fe2686ae88b677525f8e
bksarthak <bksarthak@gmail.com>
2014-12-05 00:17:59 -0800

    Update and rename cmdLineParser.py to sort.py

--- sort.py
+++ sort.py
@@ -1,6 +1,7 @@
 #This file has been created to try out a command line parser in Python.
 
 def main():
-  
+  if len(sys.argv)!=3:
+    print 'Incorrect usage: Use like "./cmdLineParser.py -
 if _name_ == '_main_':
   main()
