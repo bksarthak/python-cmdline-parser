22b950d9de8d0bb972ae1cb4cff2650b08486d9c
bksarthak <bksarthak@gmail.com>
2014-12-05 00:26:51 -0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -2,6 +2,17 @@
 
 def main():
   if len(sys.argv)!=3:
-    print 'Incorrect usage: Use like "./cmdLineParser.py -
+    print 'Incorrect usage: Use like "./sort.py {-o|--output|-r|--reverse} input filename {output filename}"'
+    sys.exit(1)
+  flag = sys.argv[1]
+  inputfile = sys.argv[2]
+  if (flag=='-o' or flag=='--output'):
+    write_output(inputfile)
+  elif (flag=='-r' or flag=='--reverse'):
+    print_reverse(inputfile,outputfile)
+  else:
+    print('Error with option entered')
+    sys.exit(1)
+    
 if _name_ == '_main_':
   main()
